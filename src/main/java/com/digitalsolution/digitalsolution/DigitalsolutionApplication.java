c7493b66ad61b4e3300ce286e4f9bd299aedc5a5
Elkin Aguilar <aguilar.elkin99@gmail.com>
2022-08-20 20:33:27 -0500

    Create proyect DigitalSoluti0n

---
+++ src/main/java/com/digitalsolution/digitalsolution/DigitalsolutionApplication.java
@@ -0,0 +1,13 @@
+package com.digitalsolution.digitalsolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalsolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalsolutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalsolution/digitalsolution/DigitalsolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalsolution.digitalsolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalsolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
